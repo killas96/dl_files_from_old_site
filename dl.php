31f8098cfbe99512b987a2282db68cbda2e60175
killas96 <60001978+killas96@users.noreply.github.com>
2022-03-18 11:23:41 +0500

    Update dl.php

--- dl.php
+++ dl.php
@@ -1,10 +1,19 @@
 <?php
 $urls = ''; # urls by \n
-$root = $_SERVER['DOCUMENT_ROOT']; # root folder start
+$root = $_SERVER['DOCUMENT_ROOT'];
+
+$arrContextOptions=array(
+    "ssl"=>array(
+        "verify_peer"=>false,
+        "verify_peer_name"=>false,
+    ),
+);  
 $arrUrls = explode("\n", trim($urls));
 foreach($arrUrls as $url){
     $url = trim($url);
-    $file = file_get_contents($url);
+	if(empty($url))
+		continue;
+    $file = file_get_contents($url, false, stream_context_create($arrContextOptions));
     $path = parse_url($url, PHP_URL_PATH);
 	$arrPath = explode("/", trim($path));
 	$filename = $arrPath[count($arrPath)-1];
